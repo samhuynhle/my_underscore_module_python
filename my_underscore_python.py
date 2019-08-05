8a2971339d9f8028138c66a6a7417d0e3eecefc0
Sam H Le <samhuynhle@gmail.com>
2019-08-05 14:40:37 -0700

    Progress

--- my_underscore_python.py
+++ my_underscore_python.py
@@ -1,19 +1,31 @@
 class Underscore:
     def map(self, iterable, callback):
-        # your code here
+        for x in range(len(iterable)):
+            iterable[x] = callback(iterable[x])
+        return iterable
+
     def find(self, iterable, callback):
-        # your code here
+        for x in range(len(iterable)):
+            if (callback(iterable[x])) == True:
+                return iterable[x]
+            
     def filter(self, iterable, callback):
-        # your code
+        new_list =[]
+        for x in range(0, len(iterable), 1):
+            if (callback(iterable[x])) == True:
+                new_list.append(iterable[x])
+        return new_list
+
     def reject(self, iterable, callback):
-        # your code
+        
+        
 # you just created a library with 4 methods!
 # let's create an instance of our class
 _ = Underscore() # yes we are setting our instance to a variable that is an underscore
-evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+# evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # should return [2, 4, 6] after you finish implementing the code above
 
-_.map([1,2,3], lambda x: x*2) # should return [2,4,6]
-_.find([1,2,3,4,5,6], lambda x: x>4) # should return the first value that is greater than 4
-_.filter([1,2,3,4,5,6], lambda x: x%2==0) # should return [2,4,6]
-_.reject([1,2,3,4,5,6], lambda x: x%2==0) # should return [1,3,5]
+print(_.map([1,2,3], lambda x: x*2)) # should return [2,4,6]
+print(_.find([1,2,3,4,5,6], lambda x: x>4)) # should return the first value that is greater than 4
+print(_.filter([1,2,3,4,5,6], lambda x: x%2==0)) # should return [2,4,6]
+print(_.reject([1,2,3,4,5,6], lambda x: x%2==0)) # should return [1,3,5]
